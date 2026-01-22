1fe650f094498fa49c6195f504f0fb5ecb9bdcbf
PTT <saobangxz1@gmail.com>
2026-01-22 20:43:20 +0700

    feat : weight game btn back

---
+++ Assets/_Project/_Script/BtnBack.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PTT;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BtnBack : ExtendMonoBehaviour
+{
+    //===================================================================== Variables
+    [SerializeField] private string _sSceneName;
+    [SerializeField] private Button _btnBack;   
+    //===================================================================== Unity Methods
+    protected override void Awake()
+    {
+        base.Awake();
+        _btnBack.onClick.AddListener(OnClickBtn);
+    }
+    //===================================================================== Action Handle
+
+    private void OnClickBtn()
+    {
+        SceneManager.UnloadSceneAsync(_sSceneName);        
+    }
+    
+    //===================================================================== Local Methods
+}
